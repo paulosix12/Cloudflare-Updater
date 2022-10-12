87b97df7f48afa7f5107046add2de520cab96e55
Paulo Eduardo <paulo_edu_12@hotmail.com>
2022-10-12 00:04:01 -0300

    Adicionado dockerfile e vars de ambiente

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-// var cron = require('node-cron');
+const path = require('path');
+const cron = require('cron').CronJob;
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
 const axios = require('axios');
-const { Console } = require('console');
-const fs = require('fs');
-const Token = '933f665692a1996f711cb4a462bb411c71688'; //DEFINIR NO DOTENV
-const domain = 'pauloedu.com.br'; //DEFINIR NO DOTENV
-var subdomains = ['vpn.pauloedu.com.br','sixterserver.pauloedu.com.br'] //DEFINIR NO DOTENV
-const email = '' //TODO DEFINIR NO DOTENV
 
-
+const domain = process.env.CLOUDFLARE_DOMAIN; 
+const subdomains = process.env.CLOUDFLARE_SUBDOMAIN.split(",");;
 const apiEndPt = 'https://api.cloudflare.com/client/v4';
-axios.defaults.headers.common['X-Auth-Email'] = 'paulo_edu_12@hotmail.com';
-axios.defaults.headers.common['X-Auth-Key'] =  Token;
-axios.defaults.headers.common['Content-Type'] = 'application/json';
 
+
+axios.defaults.headers.common['X-Auth-Email'] = process.env.CLOUDFLARE_EMAIL;
+axios.defaults.headers.common['X-Auth-Key']   = process.env.CLOUDFLARE_TOKEN;
+axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 var ip_atual;
 var zone_id;
@@ -20,25 +18,19 @@ var zone_id;
 async function getMyIp(){
   await axios.get('https://api.ipify.org/')
   .then(function (response) {
-    // manipula o sucesso da requisição
     ip_atual = response.data;
   })
   .catch(function (error) {
-    // manipula erros da requisição
     console.error(error);
   })
-  .then(function () {
-    // sempre será executado
-  });
 }
 
 async function getDnsZone(){
-  await axios.get(apiEndPt + '/zones?name=pauloedu.com.br&status=active')
+  await axios.get(apiEndPt + `/zones?name=${domain}&status=active`)
   .then(function (response) {
     return zone_id = response.data.result[0].id
   })
   .catch(function (error) {
-    // manipula erros da requisição
     console.error(error);
   })
 }
@@ -51,42 +43,37 @@ async function getDnsIP(zone_id){
     await axios.get(apiEndPt + `/zones/${zone_id}/dns_records?type=A&name=${subdomain}`)
     .then((response) =>{
       //TODO: CRIAR UMA VALIDACAO PARA SE O SUBDOMAIN IS NULL (NAO EXISTE NO CLOUDFLARE)
-      if(response.data.result[0].content != ip_atual){
+      if(response.data.result[0].content == null){
+        console.log('O Subdomin nao existe');
+      }
+      else if(response.data.result[0].content != ip_atual){
         updateDnsIP(zone_id,response.data.result[0])
         console.log(`${ts}: O IP para o ${subdomain} foi atualizado de ${response.data.result[0].content} para ${ip_atual}`)
       }else{
        console.log(`${ts}: O IP para o ${subdomain} nao precisou de IP para ${ip_atual}`)
       }
+    }).catch(function (error){
+      console.log('error');
     })
   })
 }
 
 async function updateDnsIP(zone_id,obj){
-try {
   await axios.put(apiEndPt + `/zones/${zone_id}/dns_records/${obj.id}`,
   {
-    "type":"A",
-    "name": obj.name,
-    "content": ip_atual,
-    "ttl":3600,
-    "proxied":false
-  }).then(()=>{
-    console.log(`atualizouuuu`)
+    type:"A",
+    name: obj.name,
+    content: ip_atual,
+    ttl:3600,
+    proxied:false
+  }).catch(function(error){
+    console.log('aqio');
+    console.log(error);
   })
-  
-} catch (error) {
-  console.log(error)
-} 
-  // await axios.get(apiEndPt + `/zones/${zone_id}/dns_records?type=A&name=${subdomain}`)
-  // .then(()=>{
-
-  // })
 }
 
 var start = async () =>{
 
-
-  //Get my actual IP
   await getMyIp();
 
   await getDnsZone();
@@ -95,8 +82,8 @@ var start = async () =>{
 
 }
 
-start();
+var jobUpdate = new cron('*/30 * * * *', () => {
+  start();
+});
 
-// cron.schedule('*/30 * * * *', () => {
-//   start();
-// });
+jobUpdate.start();
